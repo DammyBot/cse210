1bf3fe304415ce8f26cc50525d184dfb1195514c
Oluwadamilare2003 <oluwadamilareobalana@gmail.com>
2022-10-08 21:41:17 +0100

    writing the code again to be sure I really understand

---
+++ hilo_practice.py
@@ -0,0 +1,31 @@
+import random
+# Hilo game steps
+"""
+The player starts the game with 300 points.
+Individual cards are represented as a number from 1 to 13.
+The current card is displayed.
+The player guesses if the next one will be higher or lower.
+The the next card is displayed.
+The player earns 100 points if they guessed correctly.
+The player loses 75 points if they guessed incorrectly.
+If a player reaches 0 points the game is over.
+If a player has more than 0 points they decide if they want to keep playing.
+If a player decides not to play again the game is over.
+"""
+
+def main():
+    """
+    The main part of the code that calls the classes and other parts of the code
+    """
+
+class Card():
+    # Picks a random card number between 1 and 13
+    def display_card():
+        picked_card = random.randint(1, 13)
+        return picked_card
+
+class Output():
+    pass
+
+class ComputeResult():
+    pass
